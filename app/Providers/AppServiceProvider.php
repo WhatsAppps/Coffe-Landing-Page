9328386998da0703433affd4b0906e77d80977d3
WhatsAppps <reyyogajeemtd@gmail.com>
2024-12-13 02:30:39 +0700

    Vercels

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+   use Illuminate\Pagination\Paginator;
+   use Illuminate\Support\Facades\Blade;
+   use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+public function register()
+{
+    if (env('APP_ENV') !== 'local') {
+        URL::forceScheme('https');
     }
+}
 
     /**
      * Bootstrap any application services.
